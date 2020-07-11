1f25f394561d8038d1bcf7192b198c391a68f0ca
Jesse Audije <audijej@gmail.com>
2020-07-11 00:07:49 -0700

    functioning handlebars that serves back user data

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-const sequelize = new Sequelize("movies_db", "root", "#Nts44cqa@", {
+const sequelize = new Sequelize("movies_db", "root", "", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
