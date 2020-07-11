ba644487df23cb385719c4698e7729318f5d703c
Joe Brimeyer <jbrimeyer@gmail.com>
2020-07-11 11:42:47 -0700

    created pesistence in our database

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
-USE movies_db;
+-- USE movies_db;
 
-DROP TABLE IF EXISTS `movies`;
+-- DROP TABLE IF EXISTS `movies`;
 
 
-CREATE TABLE movies (
-  id int AUTO_INCREMENT NOT NULL,
-  routeName varchar(255) NOT NULL,
-  name varchar(255) NOT NULL,
-  catharsis int NOT NULL,
-  trigger_rating int NOT NULL,
-  comments varchar(255) NOT NULL,
-  PRIMARY KEY(id)
-);
+-- CREATE TABLE movies (
+--   id int AUTO_INCREMENT NOT NULL,
+--   routeName varchar(255) NOT NULL,
+--   name varchar(255) NOT NULL,
+--   catharsis int NOT NULL,
+--   trigger_rating int NOT NULL,
+--   comments varchar(255) NOT NULL,
+--   PRIMARY KEY(id)
+-- );
 
