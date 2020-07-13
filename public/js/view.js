0a9403878048aab0441271f11d7477bc29129326
Jesse Audije <audijej@gmail.com>
2020-07-12 18:56:56 -0700

    several updates to UI, styling, and routing

--- public/js/view.js
+++ public/js/view.js
@@ -14,14 +14,14 @@ $("#search-button").on("click", event => {
     $("#well-section").empty();
     if (!data) {
       $("#well-section").append(
-        "<h2> The movie you are searching for could not be found. </h2>"
+        "<h2> The movie you are searching for currently doesn't have any reviews. </h2>"
       );
     } else {
       $("#well-section").append("<h2>" + data.name + "</h2>");
       $("#well-section").append("<h3>Catharsis: " + data.catharsis + "</h3>");
       $("#well-section").append("<h3>Trigger: " + data.trigger_rating + "</h3>");
       $("#well-section").append("<h3>Comments: " + data.comments + "</h3>");
-      $("#well-section").append("<h3>Comments: " + data.createdAt + "</h3>");
+      $("#well-section").append("<h3>Created: " + data.createdAt + "</h3>");
     }
   });
 
